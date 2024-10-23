ad75f8ed25e6e46167b52b2f37a3ddc2899d7c59
user <dolmatova3010@yandex.ru>
2024-10-23 22:53:59 +0700

    Д/3

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,10 +3,14 @@ from gettext import Catalog
 from django.urls import path
 from . import views  # Импортируем views
 from .apps import CatalogConfig
+from .views import product_detail, product_list
+from catalog.views import index
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', views.home, name='home'),  # Главная страница
     path('contact/', views.contact, name='contact'),  # Страница контактов
+    path('product/<int:pk>/', product_detail, name='product_detail'),
+    path('products/', product_list, name='product_list'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 
 def contact(request):
-    return render(request, 'catalog/contact.html')
+    return render(request, 'contact.html')
 
 
 def home(request):
-    return render(request, 'catalog/home.html')
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'home.html', context)
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'product_detail.html', context)
+
+def product_list(request):
+    pass
+
+def index(request):
+    return render(request, 'base.html')
